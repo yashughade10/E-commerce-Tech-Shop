6a9faa11d7532bb23522b31669fd9077567e11ea
Yashwant <yashwantughade10@gmail.com>
2024-04-25 16:03:54 +0530

    Final changes

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -71,7 +71,7 @@ const Footer = () => {
             <div className="foot_copyright">
               <p>
                 {currYear} | All Rights Reserved. Built by |{" "}
-                <a href="https://gulshansongara.netlify.app/">YASHWANT UGHADE</a>
+                <a href="https://github.com/yashughade10">YASHWANT UGHADE</a>
               </p>
             </div>
             <div className="foot_social">
